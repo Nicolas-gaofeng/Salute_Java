aaabdcbf5698ca2453a0697b2b960c99c97b7cd5
zgf <396607662@qq.com>
2020-12-30 23:50:38 +0800

    init java

---
+++ src/java/basic/HelloWorld.java
@@ -0,0 +1,20 @@
+package src.java.basic;
+
+/**
+ * @author : zgf
+ * @file : HelloWorld.java
+ * @brief : 第一个java程序
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
+
+
+
+
+
+
---
+++ src/java/basic/JavaDebug.java
@@ -0,0 +1,19 @@
+package src.java.basic;
+
+/**
+ * @author : zgf
+ * @file : JavaDebug.java
+ * @brief : debug
+ */
+
+public class JavaDebug {
+    /*
+        这是main方法
+        main方法是程序的入口方法，代码的执行是从main方法开始的
+    */
+    public static void main(String[] args) {
+        // 这是输出语句，""里面的内容是可以改变的
+        System.out.println("JavaDebug");
+        //  System.out.prinltln("JavaDebug");
+    }
+}
