cbc77e47b3166cfece3b4367a25822dbdddd24c7
zgf <396607662@qq.com>
2021-02-05 16:17:16 +0800

    init

--- src/basic/helloworld/HelloWorld.java
+++ src/basic/helloworld/HelloWorld.java
@@ -6,15 +6,8 @@ package src.java.basic.helloworld;
  * @brief : 第一个java程序
  */
 
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
     }
 }
-
-
-
-
-
-
